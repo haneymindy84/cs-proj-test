6f3c2b557f2c9b485dbc5ecc1e96848efa22c0f9
Jessica Lord <jlord@glitch.com>
2020-03-25 13:41:24 -0400

    I did another

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import "./styles.css";
 document.getElementById("app").innerHTML = `
 <h1>Hello I changed this!</h1>
 <div>
-  We use Parcel to bundle this sandbox, you can find more info about Parcel
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+  <p>Blah blah blah blah</p>
 </div>
 `;
